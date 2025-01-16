c6c4baba9aa56454e6bd605844c42c8e7354f2e9
romeluis <113552563+romeluis@users.noreply.github.com>
2025-01-16 13:51:09 -0500

    Added support

--- src/components/EventInfo.js
+++ src/components/EventInfo.js
@@ -72,6 +72,11 @@ class EventDisplay {
             desc=[{type: "long-text", title: "Description", body: eventInfo.description, colour: "black"}];
         }
 
+        let calLink = []
+        if (eventInfo.calendarLink !== "NONE") {
+            button = {type: "button", title: "Calendar Invite", body: <ButtonRedirect bold text="Add to Calendar" fontSize="1.25rem" colour={this.colour}  buttonStyle="fill" useIcon icon={icon} linkTo={eventInfo.calendarLink}/>, colour: "black"};
+        }
+
         //Compile all the info
         this.pageInfo = title.concat(org, date, time, location, price, button, desc);
         this.cpif = eventInfo.isCpsifFunded === 1
